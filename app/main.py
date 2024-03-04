8d382ef8da3329f92d5993b84246acb23713d7ee
martindevelopske <martindevelopske@gmail.com>
2024-03-04 21:06:38 +0300

    added ORM

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+from . import models
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
 
 
 my_posts=[{"id": 1, "title": "Nakamoto", "content": "hello from post"}, {"id": 2, "title": "post tetatet", "content": "hello from post"}]
@@ -17,7 +23,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
 
 
 db_user = os.environ.get('DB_USER')
@@ -26,14 +32,14 @@ db_host = os.environ.get('DB_HOST')
 db_port = os.environ.get('DB_PORT')
 db_name = os.environ.get('DB_NAME')
 
-try:
-    # conn= psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_password, cursor_factory=RealDictCursor, )
-    conn = psycopg2.connect("dbname=fast user=martin port=5432 password=password")
-    cursor=conn.cursor()
-    print("Database connection successfull")
-except Exception as error:
-    print("connecting to database failed")
-    print(error)
+# try:
+#     # conn= psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_password, cursor_factory=RealDictCursor, )
+#     conn = psycopg2.connect("dbname=fast user=martin port=5432 password=password")
+#     cursor=conn.cursor()
+#     print("Database connection successfull")
+# except Exception as error:
+#     print("connecting to database failed")
+#     print(error)
 
 def get_post_by_id(posts, post_id):
     for post in posts:
@@ -49,49 +55,52 @@ def get_index(id):
 
 
 @app.get("/")
-async def root():
+async def root(db: Session= Depends(get_db)):
     return {"message": "welcome to my api blud!!"}
 
 @app.get("/posts")
-async def get_posts():
+async def get_posts(db:Session= Depends(get_db)):
+    posts= db.query(models.Post).all()
     return {"posts": my_posts}
 
 @app.get("/posts/{id}")
-async def get_posts(id: int, res: Response):
-    print(id)    
-    post= get_post_by_id(my_posts, id)
+async def get_posts(id: int, res: Response, db:Session= Depends(get_db)):
+    post= db.query(models.Post).filter(models.Post.id==id).first()
     if post: 
         return {"POST": post}
     # res.status_code=status.HTTP_404_NOT_FOUND
     # return {"post": "post not found"}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post with that ID does not exist")
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post, res:Response):
+async def create_post(post: Post, res:Response, db:Session= Depends(get_db)):
     #title str, content str, category, bool published/draft
     
-    print(post)
-    print(post.model_dump())
     post_dict= post.model_dump()
-    post_dict["id"]= randrange(0, 100000)
-    my_posts.append(post_dict)
-    return {"message": "successfully created", "post": post_dict}
+    new_post= models.Post(**post_dict)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"message": "successfully created", "post": new_post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int):
-   post= get_post_by_id(my_posts, id)
-   if post:
-       index =get_index(id)
-       my_posts.pop(index)
+async def delete_post(id: int, db:Session= Depends(get_db)):
+   query= db.query(models.Post).filter(models.Post.id==id)
+   if query.first() is not None:
+       query.delete(synchronize_session=False)
+       db.commit()
        return {"message": "post was successfully deleted"}  
    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post does not exist")
        
 @app.put("/posts/{id}")
-async def update_post(id:int, post: Post):
-    oldPostIndex= get_index(id)
-    if not oldPostIndex:
+async def update_post(id:int, postdata: Post, db:Session= Depends(get_db)):
+    query=db.query(models.Post).filter(models.Post.id == id)
+    post=query.first()
+    post_dict= postdata.model_dump()
+
+    if not post:
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post does not exist.")
-    post_dict= post.model_dump()
-    post_dict["id"]=id
-    my_posts[oldPostIndex]= post_dict
-    return {"data": post_dict}
+    query.update(post_dict, synchronize_session=False)
+    db.commit()
+    
+    return {"data": 'done...', "data": query.first()}
